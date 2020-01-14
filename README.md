a88744110c8120c3edeea64e881bed49eaf95b50
Matt Benson <rabenson6@msn.com>
2020-01-13 19:08:40 -0800

    final version for submission

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # hw5
+Day Planner
+
+Purpose:
+To allow the user to plan out their day in a concise and organized location
+
+Features:
+Input and save functions so the user can keep track of their tasks for the current day
+Current time block is highlighted red so the user will know what they should currently be
+doing, and past events will turn grey. Any future time blocks will appear green.
+
+Instructions:
+At the start of the day, the user enters their schedule for the day into a time block.
+The user then clicks the adjacent "save" button to save the content to local storage for 
+later recall.
+If any revisions need to be made, simply click the text field, make your changes and click
+save again.
--- script.js
+++ script.js
@@ -14,31 +14,31 @@ let Task;
 let Time;
 
 // display current date
-var headerdate = moment().format(" dddd, MMMM Do");
+var headerdate = moment().format(" dddd,  MMMM Do");
 dateBox.innerHTML = "<p> Current Date: " + headerdate +"</p>";
 
-    $(document).ready(function() {
-    var retrievedTask = JSON.parse(localStorage.getItem("4PM Input"))
-    console.log(retrievedTask["Task"]);
-    $("#input16").val(retrievedTask["Task"]);
-    });
 
 
 
 
-// functions to change the content box colors based on the time
+// functions to change the content box colors based on the time change var currenthour to a number between 9 and 17 to see 
+// how it acts at different times of day
 var date = new Date();
-var currentHour = 15
-// date.getHours();
+var currentHour = date.getHours();
 var hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+
+// On load, this function retrieves any saved tasks
+hoursArray.forEach(function(element){
+$(document).ready(function() {
+    var retrievedTask = JSON.parse(localStorage.getItem(element +" Task"))
+    $("#input"+element).val(retrievedTask["Task"]);
+    })});
 
 // This function checks the time and changes the colors of the text inputs based on the time of day
    function checktime() 
    {hoursArray.forEach(function(element){
     if (currentHour > element) {
         $(eval("Box" + element)).addClass("pastcontentbox").removeClass("contentbox");
-        $(eval("savebox" + element)).hide("savebox" + element);
-        $(eval("input" + element)).hide(".input" + element);
     } 
     
     else if (currentHour === element) {
@@ -49,18 +49,80 @@ var hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17]
         
     })};
 
-    // Save task to Local Storage
-    
+    // Save tasks to Local Storage
+    $('#savebox9').on('click', function(){
+        var storedTask = {
+        Time: "9AM",
+        Task: $("#input9").val().trim(),
+        
+        } 
+        localStorage.setItem("9 Task", JSON.stringify(storedTask));
+    });
+    $('#savebox10').on('click', function(){
+        var storedTask = {
+        Time: "10AM",
+        Task: $("#input10").val().trim(),
+        
+        } 
+        localStorage.setItem("10 Task", JSON.stringify(storedTask));
+    });
+    $('#savebox11').on('click', function(){
+        var storedTask = {
+        Time: "11AM",
+        Task: $("#input11").val().trim(),
+        
+        } 
+        localStorage.setItem("11 Task", JSON.stringify(storedTask));
+    });
+    $('#savebox12').on('click', function(){
+        var storedTask = {
+        Time: "12PM",
+        Task: $("#input12").val().trim(),
+        
+        } 
+        localStorage.setItem("12 Task", JSON.stringify(storedTask));
+    });
+    $('#savebox13').on('click', function(){
+        var storedTask = {
+        Time: "1PM",
+        Task: $("#input13").val().trim(),
+        
+        } 
+        localStorage.setItem("13 Task", JSON.stringify(storedTask));
+    });
+    $('#savebox14').on('click', function(){
+        var storedTask = {
+        Time: "2PM",
+        Task: $("#input14").val().trim(),
+        
+        } 
+        localStorage.setItem("14 Task", JSON.stringify(storedTask));
+    });
+    $('#savebox15').on('click', function(){
+        var storedTask = {
+        Time: "3PM",
+        Task: $("#input15").val().trim(),
+        
+        } 
+        localStorage.setItem("15 Task", JSON.stringify(storedTask));
+    });
     $('#savebox16').on('click', function(){
         var storedTask = {
         Time: "4PM",
         Task: $("#input16").val().trim(),
-
+        
         } 
-        localStorage.setItem("4PM Input", JSON.stringify(storedTask));
-           
-        }
-    );
+        localStorage.setItem("16 Task", JSON.stringify(storedTask));
+    });
+    $('#savebox17').on('click', function(){
+        var storedTask = {
+        Time: "5PM",
+        Task: $("#input17").val().trim(),
+        
+        } 
+        localStorage.setItem("17 Task", JSON.stringify(storedTask));
+    });
+
 
     function loadTasks(){
         var retrievedTask = JSON.parse(localStorage.getItem(storedTask))
@@ -68,9 +130,18 @@ var hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
 checktime();
 
-// some fun post 5 PM scripts
-if(currentHour >= 17){
-    $(Box17).append("<p>Work Day is over, time for Happy Hour! </p>");
-    $(savebox17).append("Open Beer");
-}
+// some fun post 5 PM scripts I was working on, trying to make a beer finder through google maps
+// if(currentHour >= 17){
+//     $(".col-md-10").prepend("<button class = 'beerFinder'>Find Nearest Happy Hour</button>")
+//     $(".col-md-10").prepend("<div> Work Day is over, time for Happy Hour! </div>");
+//     $(".col-md-10").prepend("<div id= 'map'style='width:100%;height:100%;'></div>");
+    
+// }
 
+// $(".beerFinder").on('click',function myMap() {
+//     var mapProp= {
+//         center:new google.maps.LatLng(51.508742,-0.120850),
+//         zoom:12,
+//       };
+//       var map = new google.maps.Map(document.getElementById("map"),mapProp);
+//       });
